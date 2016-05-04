539aada695c8c08e1772151194f45751d4b2adde
YaminLi <yaminli2016@u.northwestern.edu>
2016-05-03 22:54:31 -0500

    add import page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -38,13 +38,47 @@ angular.module('starter.controllers', [])
       $scope.closeLogin();
     }, 1000);
   };
+
 })
 
-.controller('PlaylistsCtrl', function($scope, $http) {
-  $http.get("https://api.pinterest.com/v1/boards/amyilyse/interiors/pins/?access_token=AX0EL2K3PBu3ZineycN4SYBiZiahFEsiwPji579DEIReRwBBUQAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cboard%2Cimage%2Ccreated_at%2Ccreator%2Cattribution").then(function(response){
-    console.log(response.data.data);
-    $scope.items = response.data.data;
+
+
+.controller('PlaylistsCtrl', function($scope, $ionicModal, $timeout, $http) {
+
+  $scope.importData = {}
+
+  $ionicModal.fromTemplateUrl('templates/import.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.importModal = modal;
   });
+
+  $scope.closeImport = function() {
+    $scope.importModal.hide();
+  };
+
+  // Open the login modal
+  $scope.import = function() {
+    $scope.importModal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doImport = function() {
+    console.log('Doing Import', $scope.importData);
+    $http.get("https://api.pinterest.com/v1/boards/"+$scope.importData.username+"/"+$scope.importData.boardname+"/pins/?access_token=AX0EL2K3PBu3ZineycN4SYBiZiahFEsiwPji579DEIReRwBBUQAAAAA&fields=id%2Clink%2Cnote%2Curl%2Cboard%2Cimage%2Ccreated_at%2Ccreator%2Cattribution").then(function(response){
+      console.log(response.data.data);
+      $scope.items = response.data.data;
+    });
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeImport();
+    }, 1000);
+  };
+
+  // console.log($scope.importData)
+
+  
   // $scope.playlists = [
   //   { title: 'Reggae', id: 1 },
   //   { title: 'Chill', id: 2 },
