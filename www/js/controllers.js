ac09563a38727b7f48917f66615c607f5f0ea8d1
Kuba Jezusek <jakub.jezusek@gmail.com>
2016-05-25 21:00:48 -0500

    Gridster
    Trying to make the masonry layout work with angular gridster

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -194,34 +194,38 @@ angular.module('starter.controllers', [])
 
       // listen for drop related events:
 
-      ondropactivate: function (event) {
-        // add active dropzone feedback
-        event.target.classList.add('drop-active');
-      },
-      ondragenter: function (event) {
-        var draggableElement = event.relatedTarget,
-            dropzoneElement = event.target;
-
-        // feedback the possibility of a drop
-        dropzoneElement.classList.add('drop-target');
-        draggableElement.classList.add('can-drop');
-        draggableElement.textContent = 'Dragged in';
-      },
-      ondragleave: function (event) {
-        // remove the drop feedback style
-        event.target.classList.remove('drop-target');
-        event.relatedTarget.classList.remove('can-drop');
-        event.relatedTarget.textContent = 'Dragged out';
-      },
-      ondrop: function (event) {
-        event.relatedTarget.textContent = 'Dropped';
-      },
-      ondropdeactivate: function (event) {
-        // remove active dropzone feedback
-        event.target.classList.remove('drop-active');
-        event.target.classList.remove('drop-target');
-      }
     });
+
+    $scope.gridsterOpts = {
+    columns:2,// the width of the grid, in columns
+    pushing: true, // whether to push other items out of the way on move or resize
+    floating: true, // whether to automatically float items up so they stack (you can temporarily disable if you are adding unsorted items with ng-repeat)
+    swapping: false, // whether or not to have items of the same size switch places instead of pushing down if they are the same size
+    width: 'auto', // can be an integer or 'auto'. 'auto' scales gridster to be the full width of its containing element
+    colWidth: 'auto', // can be an integer or 'auto'.  'auto' uses the pixel width of the element divided by 'columns'
+   rowHeight: 'match', // can be an integer or 'match'.  Match uses the colWidth, giving you square widgets.
+    margins: [0, 0], // the pixel distance between each widget
+    outerMargin: true, // whether margins apply to outer edges of the grid
+    isMobile: false, // stacks the grid items if true
+    mobileBreakPoint: 600, // if the screen is not wider that this, remove the grid layout and stack the items
+    mobileModeEnabled: false, // whether or not to toggle mobile mode when screen width is less than mobileBreakPoint
+    minColumns: 1, // the minimum columns the grid must have
+    minRows: 2, // the minimum height of the grid, in rows
+    maxRows: 100,
+    defaultSizeX: 1, // the default width of a gridster item, if not specifed
+    defaultSizeY: 1, // the default height of a gridster item, if not specified
+    minSizeX: 1, // minimum column width of an item
+    maxSizeX: null, // maximum column width of an item
+    minSizeY: 1, // minumum row height of an item
+    maxSizeY: null, // maximum row height of an item
+    resizable: {
+       enabled: true,
+       handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw'],
+       start: function(event, $element, widget) {}, // optional callback fired when resize is started,
+       resize: function(event, $element, widget) {}, // optional callback fired when item is resized,
+       stop: function(event, $element, widget) {} // optional callback fired when item is finished resizing
+    }
+};
 
     window.dragMoveListener = dragMoveListener;
 
